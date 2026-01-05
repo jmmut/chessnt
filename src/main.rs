9273da3c2b76ae55fa447a8ad6db9009dc3304b5
jose miguel mut <jomutlo@gmail.com>
2026-01-06 00:25:07 +0100

    start drawing board

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
+use juquad::draw::draw_segment;
+use juquad::lazy::add_contour;
 use macroquad::prelude::*;
+
+pub const COLUMNS: i32 = 8;
+pub const ROWS: i32 = 8;
+
+pub struct Coord {
+    column: i32,
+    row: i32,
+}
+pub fn coord_to_pixel(coord: Coord, rect: Rect) -> Vec2 {
+    let cell_width = rect.w / COLUMNS as f32;
+    let cell_height = rect.h / ROWS as f32;
+    vec2(cell_width * coord.column as f32, cell_height*coord.row as f32)
+}
 
 #[macroquad::main("MY_CRATE_NAME")]
 async fn main() {
     loop {
-        clear_background(RED);
-
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
-
+        clear_background(LIGHTGRAY);
+        
+        if is_key_pressed(KeyCode::Escape) {
+            return;
+        }
+        let screen = Vec2::new(screen_width(), screen_height());
+        let rect = add_contour(Rect::new(0.0, 0.0, screen.x, screen.y), -screen * 0.5);
+        for column in 0..=COLUMNS {
+            let start = coord_to_pixel(Coord{column, row: 0}, rect);
+            let end = coord_to_pixel(Coord{column, row: ROWS}, rect);
+            draw_segment(start, end, 2.0, DARKGRAY);
+        }
+        // for column in 0..=COLUMNS {
+        //     let start = coord_to_pixel(Coord{column, row: 0}, rect);
+        //     let end = coord_to_pixel(Coord{column, row: ROWS}, rect);
+        //     draw_segment(start, end, 2.0, DARKGRAY);
+        // }
+        
         next_frame().await
     }
 }
