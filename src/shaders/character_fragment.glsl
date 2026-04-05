1b16f2129fde290bf7b6868319e33144c44a6a73
jose miguel mut <jomutlo@gmail.com>
2026-04-05 19:15:13 +0200

    simplify palette shader

--- src/shaders/character_fragment.glsl
+++ src/shaders/character_fragment.glsl
@@ -7,14 +7,13 @@ varying vec2 uv;
 uniform sampler2D Texture;
 
 void main() {
-    vec3 code = vec3(241.0 / 255.0, 1.0, 135.0/255.0);
+    vec3 code = vec3(0.0, 1.0, 1.0); // cyan
     vec4 sampled = texture2D(Texture, uv);
+    vec3 diff = abs(code - sampled.rgb);
+    float dist = length(diff);
     
-    vec3 p = sampled.rgb / code;  // NOTE: division by 0!
-    float p_avg = (p.r + p.g + p.b) / 3.0;
-    float dist = abs(p.r - p_avg) + abs(p.g - p_avg) + abs(p.b - p_avg);
-    if (dist < 0.05 && p_avg > 0.1) {
-        vec3 color_out = sampled.rgb * dist + color.rgb * p * (1.0 - dist);
+    if (dist < 0.01) {
+        vec3 color_out = color.rgb;
         gl_FragColor = vec4(color_out, sampled.a);
     } else {
         gl_FragColor = sampled;
