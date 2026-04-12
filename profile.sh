90d22af215c44c559235a4f79ffcfda2e56b1fe9
jose miguel mut lopez <jose.lopez@ipsos.com>
2026-04-12 17:20:47 +0200

    backup previous profile data

--- profile.sh
+++ profile.sh
@@ -2,4 +2,6 @@
 
 cargo t --no-run --profile profiling
 
+mv optimising/profile.json.gz optimising/previous-profile.json.gz
+
 samply record -o optimising/profile.json.gz cargo test --lib chessnt --profile profiling -- --no-capture --ignored benchmark
